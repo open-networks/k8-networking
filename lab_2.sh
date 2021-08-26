6fc344f5e64166df9413d5d68c5c242eb0e0a971
fkain <fk@ong.at>
2021-08-26 02:18:07 +0200

    fix

--- lab_2.sh
+++ lab_2.sh
@@ -2,81 +2,70 @@
 
 source 0_init.sh
 printf "\n========================\n"
-printf "Let's explore those Pods"
+printf "Let's explore service"
 printf "\n========================\n"
-printf "\nshow the pods network interfaces and address\n"
-read -p 'kubectl exec -it $POD1 -- ip address show'
-kubectl exec -it $POD1 -- ip address show
 
-printf "\nshow the pods routes\n"
-read -p 'kubectl exec -it $POD1 -- route'
-kubectl exec $POD1 -- route
+
+#test service
+printf "\n inspect kube-proxy. it is running as a daemon set on each node\n"
+kubectl get daemonset kube-proxy -n kube-system
+kubectl describe pods kube-proxy -n kube-system
+
+printf "\n verify that there are no entries for the network-testing/multitool project in iptables.\n"
+read -p "kubectl exec multitool-privileged -- chroot /host iptables-save | grep 'network-testing/multitool'"
+kubectl exec multitool-privileged -- chroot /host iptables-save | grep 'network-testing/multitool'
+
+printf "\ncreate a service\n"
+read -p "kubectl expose deployment multitool --port 80"
+kubectl expose deployment multitool --port 80
+
+printf "\ninspect the service\n"
+read -p "kubectl describe service multitool"
+kubectl describe service multitool
+
+printf "\niptables rules have been added.\n"
+printf "KUBE-SERVICE entries  show starting rules when the service ip is to be reached and forward it to KUBE-SVC.\n"
+printf "KUBE-SVC entries shows the loadbalancing with by forwarding it to KUBE-SEP with the --propability flag.\n"
+printf "KUBE-SEP entries add DNAT to the coressponding Pod ip.\n"
+read -p "kubectl exec multitool-privileged -- chroot /host iptables-save | grep 'network-testing/multitool'"
+kubectl exec multitool-privileged -- chroot /host iptables-save | grep 'network-testing/multitool'
+
+
+printf "\nshow the dns service\n"
+read -p "kubectl get service kube-dns -n kube-system"
+kubectl get service kube-dns -n kube-system
+read -p "kubectl get deployment coredns -n kube-system"
+kubectl get deployment coredns -n kube-system
 
 printf "\nshow the pods dns\n"
 read -p 'kubectl exec -it $POD1 -- cat /etc/resolv.conf'
 kubectl exec $POD1 -- cat /etc/resolv.conf
 
-printf "\nshow the pods arp cache with only one entry\n"
-read -p 'kubectl exec -it $POD1 -- arp -a'
-kubectl exec $POD1 -- arp -a
-
-printf "\nping pod 2 and pod 3\n"
-read -p 'kubectl exec $POD1 -- ping -c 4 $POD2_IP'
-kubectl exec $POD1 -- ping -c 3 $POD2_IP
-kubectl exec $POD1 -- ping -c 3 $POD3_IP
-
-printf "\nshow pods arp cache with more entries. Recognize that the pods running on other machines are missing\n"
-read -p 'kubectl exec -it $POD1 -- arp -a'
-kubectl exec $POD1 -- arp -a
-
-
-printf "\n=====================\n"
-printf "lets explore the host"
-printf "\n=====================\n"
-
-printf "\nshow status and config of the kubelet service\n"
-read -p "kubectl exec -it multitool-privileged -- chroot /host systemctl status kubelet.service"
-kubectl exec -it multitool-privileged -- chroot /host systemctl status kubelet.service
-
-printf "\nshow the host interfaces and ip addresses\n"
-read -p "kubectl exec multitool-privileged -- chroot /host ip address show"
-kubectl exec multitool-privileged -- chroot /host ip address show
-
-printf "\nshow hosts route\n"
-read -p "kubectl exec multitool-privileged -- chroot /host route"
-kubectl exec multitool-privileged -- chroot /host route
-
-printf "\nshow the hosts linux bridges\n"
-read -p "kubectl exec multitool-privileged -- chroot /host brctl show"
-kubectl exec multitool-privileged -- chroot /host brctl show
-
-printf "\nexplore the kubernetes bridge\n"
-read -p "enter brdige device name:" BRIDGE
-read -p 'kubectl exec multitool-privileged -- chroot /host brctl showmacs $BRIDGE'
-kubectl exec multitool-privileged -- chroot /host brctl showmacs $BRIDGE
-
-
-printf "\n look at the cni configs\n"
-read -p 'kubectl exec multitool-privileged -- chroot /host ls /etc/cni/net.d/ -la'
-kubectl exec multitool-privileged -- chroot /host ls /etc/cni/net.d/ -la
-read -p 'kubectl exec multitool-privileged -- chroot /host cat  /etc/cni/net.d/*'
-kubectl exec multitool-privileged -- chroot /host cat $(kubectl exec multitool-privileged -- chroot /host find /etc/cni/net.d/)
-
-
-
-
 # shows dns config
+printf "\nshow dns server config\n"
+read -p 'kubectl exec -it multitool-privileged -- chroot /host cat var/lib/kubelet/config.yaml'
 kubectl exec -it multitool-privileged -- chroot /host cat var/lib/kubelet/config.yaml
 
-#test service
-read -p "kubectl exec multitool-privileged -- chroot /host iptables-save | grep 'network-testing/multitool'"
-kubectl exec multitool-privileged -- chroot /host iptables-save | grep 'network-testing/multitool'
 
-read -p "kubectl expose deployment multitool --port 80"
-kubectl expose deployment multitool --port 80
+printf "apply ingresses for multitool application"
 
-read -p "kubectl describe service multitool"
-kubectl describe service multitool
+printf '\nexpose service via LoadBalancer\n'
+read -p "kubectl expose deployment multitool --port 80 --type=LoadBalancer --name mutitool-external"
+kubectl expose deployment multitool --port 80 --type=LoadBalancer --name mutitool-external
+read -p "kubectl get service multitool-external"
+kubectl get service multitool-external
+NODE_PORT=$(kubectl get service multitool-external -o json | jq -r '.spec.ports[].nodePort')
+NODE_ADDRESS=$(kubectl get node -o json |jq -r '.items[0].status.addresses[0].address')
 
-read -p "kubectl exec multitool-privileged -- chroot /host iptables-save | grep 'network-testing/multitool'"
-kubectl exec multitool-privileged -- chroot /host iptables-save | grep 'network-testing/multitool'
+printf "\nVerify that service is accessible via node port. External ip can be assigned by the external loadbalancer manually\n"
+curl $NODE_ADDRESS:$NODE_PORT
+
+printf 'expose service via Ingress'
+read -p 'kubectl apply -f ingress_example.yaml'
+kubectl apply -f ingress_example.yaml
+
+curl $NODE_ADDRESS
+
+# istio example microservice app.
+kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/kubernetes-manifests.yaml 
+kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/istio-manifests.yaml
